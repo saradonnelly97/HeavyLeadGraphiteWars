be00c1de70b9e4515123ca60e7c145dcf0492f15
saradonnelly97 <sara.donnelly8@gmail.com>
2023-06-12 20:55:08 -0400

    added all code altered in codepen.

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var extendButton = document.getElementById('extend-button');
 var resetButton = document.getElementById('reset-button');
 var winMessage = document.getElementById('win-message');
 var finishLinePosition = 500;
+var computerPencil = document.getElementById('computer-pencil');
+
+var playerPencilHeight = 0;
+var computerPencilHeight = 0;
+var computerPencilInterval;
 
 extendButton.addEventListener('click', function () {
     var currentHeight = parseInt(playerPencil.style.height) || 0;
@@ -13,9 +18,17 @@ extendButton.addEventListener('click', function () {
     }
 });
 
-resetButton.addEventListener('click', function () {
-    resetGame();
-});
+function startComputerPencil() {
+    computerPencilInterval = setInterval(function () {
+        computerPencilHeight += 2;
+        computerPencil.style.height = computerPencilHeight + 'px';
+
+        if (computerPencilHeight >= finishLinePosition) {
+            clearInterval(computerPencilInterval);
+            loseGame();
+        }
+    }, 100);
+}
 
 function winGame() {
     playerPencil.style.height = finishLinePosition + 'px';
@@ -28,9 +41,26 @@ function winGame() {
     }, 3000);
 }
 
+function loseGame() {
+    extendButton.style.visibility = 'hidden';
+    resetButton.style.visibility = 'visible';
+    resetButton.style.display = 'block';
+}
+
+resetButton.addEventListener('click', function () {
+    resetGame();
+});
+
 function resetGame() {
     playerPencil.style.height = '3px';
+    computerPencil.style.height = '3px';
     resetButton.style.visibility = 'hidden';
     extendButton.style.visibility = 'visible';
     winMessage.style.display = 'none';
+    playerPencilHeight = 0;
+    computerPencilHeight = 0;
+    clearInterval(computerPencilInterval);
+    startComputerPencil(); // Start computer-pencil again
 }
+
+startComputerPencil();
