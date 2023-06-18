e5f0926d4e2515f1ede76e1c03d2cdef2dcdb54a
saradonnelly97 <sara.donnelly8@gmail.com>
2023-06-18 11:36:37 -0400

    working on progress bar

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ let computerPencilInterval;
 // Wait for DOMContentLoaded event to ensure all elements are available in the DOM
 window.addEventListener('DOMContentLoaded', function () {
     // Get references to DOM elements
-    playerPencil = document.getElementById('playerPencil');
-    extendButton = document.getElementById('extendButton');
-    splashPage = document.getElementById('splashPage');
-    computerPencil = document.getElementById('cpuPencil');
+playerPencil = document.getElementById('playerPencil');
+extendButton = document.getElementById('extendButton');
+splashPage = document.getElementById('splashPage');
+computerPencil = document.getElementById('cpuPencil');
 
     // Add event listener to extend button
-    extendButton.addEventListener('click', extendPlayerPencil);
+extendButton.addEventListener('click', extendPlayerPencil);
 
     // Start the computer pencil animation
-    startComputerPencil();
+startComputerPencil();
 });
 
 // Function to extend the player pencil
