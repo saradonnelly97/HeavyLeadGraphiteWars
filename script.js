b5f372186104c18e960b06d3434081efc832373d
saradonnelly97 <sara.donnelly8@gmail.com>
2023-06-19 15:44:51 -0400

    trying to get a functioning gauge again

--- script.js
+++ script.js
@@ -1,45 +1,103 @@
-let playerPencilHeight = 0;
-let computerPencilHeight = 0;
-let computerPencilInterval;
+//let playerPencilHeight = 0;
+//let computerPencilHeight = 0;
+//let computerPencilInterval;
 
 // Wait for DOMContentLoaded event to ensure all elements are available in the DOM
-window.addEventListener('DOMContentLoaded', function () {
+//window.addEventListener('DOMContentLoaded', function () {
     // Get references to DOM elements
-playerPencil = document.getElementById('playerPencil');
-extendButton = document.getElementById('extendButton');
-splashPage = document.getElementById('splashPage');
-computerPencil = document.getElementById('cpuPencil');
+//playerPencil = document.getElementById('playerPencil');
+//extendButton = document.getElementById('extendButton');
+//splashPage = document.getElementById('splashPage');
+//computerPencil = document.getElementById('cpuPencil');
 
     // Add event listener to extend button
-extendButton.addEventListener('click', extendPlayerPencil);
+//extendButton.addEventListener('click', extendPlayerPencil);
 
     // Start the computer pencil animation
-startComputerPencil();
-});
+//startComputerPencil();
+//});
 
 // Function to extend the player pencil
-function extendPlayerPencil() {
-    let currentHeight = parseInt(playerPencil.style.height) || 0;
-    playerPencil.style.height = (currentHeight + 5) + 'px';
+//function extendPlayerPencil() {
+//    let currentHeight = parseInt(playerPencil.style.height) || 0;
+//    playerPencil.style.height = (currentHeight + 5) + 'px';
 
-    if (currentHeight >= finishLinePosition) {
-        winGame();
-    }
-}
+//    if (currentHeight >= finishLinePosition) {
+//        winGame();
+//    }
+//}
 
 // Function to start the computer pencil animation
-function startComputerPencil() {
-    computerPencilInterval = setInterval(function () {
-        computerPencilHeight += 2;
-        computerPencil.style.height = computerPencilHeight + 'px';
+//function startComputerPencil() {
+//    computerPencilInterval = setInterval(function () {
+ //       computerPencilHeight += 2;
+ //       computerPencil.style.height = computerPencilHeight + 'px';
 
-        if (computerPencilHeight >= finishLinePosition) {
-            clearInterval(computerPencilInterval);
-            loseGame();
+//        if (computerPencilHeight >= finishLinePosition) {
+//            clearInterval(computerPencilInterval);
+//            loseGame();
+//        }
+//    }, 100);
+//}
+
+var score;
+var duration = 25;
+var startTime;
+var ended = true;
+
+var timerTxt = document.getElementById("timer");
+var scoreTxt = document.getElementById("score");
+var clicksTxt = document.getElementById("clicks");
+var goButton = document.getElementById("goButton");
+var clickArea = document.getElementById("clickArea");
+
+var show = function(elem) {
+    elem.style.display = 'inline';
+};
+
+var hide = function(elem) {
+    elem.style.display = 'none';
+};
+
+function startGame() {
+    hide(goButton);
+    score = -1;
+    ended = false;
+    startTime = new Date().getTime();
+    var timerId = setInterval(function() {
+        var total = (new Date().getTime() - startTime) / 1000;
+
+        if (total < duration) {
+            timerTxt.textContent = total.toFixed(2);
+        } else {
+            ended = true;
+            clearInterval(timerId);
+            endGame();
         }
-    }, 100);
+    }, 1);
 }
 
+function endGame() {
+    var clicksBySec = (score / duration).toFixed(2);
+    timerTxt.textContent = duration.toFixed(3);
+    clicksTxt.textContent = clicksBySec;
+    show(goButton);
+
+    setTimeout(function() {
+        alert('You made ' + score + ' clicks in ' + duration + ' seconds. It is ' + clicksBySec + ' clicks per second.');
+    }, 10);
+}
+
+goButton.addEventListener("click", function() {
+    startGame();
+});
+
+clickArea.addEventListener("click", function() {
+    if (!ended) {
+        score++;
+        scoreTxt.textContent = score;
+    }
+});
 
 // Function to hide the splash page
 function hideSplashPage() {
